0da4d9c3358afcdec254915550cb405ec512c9d9
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-01-03 16:22:48 +0900

    feat: 2-2-4.CSVLoader.py

--- chapter2/2-2-3.DirectoryLoader.py
+++ chapter2/2-2-3.DirectoryLoader.py
@@ -1,4 +1,4 @@
-# 2-2-1 Document Locader - Directory Loader
+# 2-2-3 Document Locader - Directory Loader
 """
 DirectoryLoader를 사용하여 디렉토리 내의 모든 문서를 로드할 수 있습니다. DirectoryLoader 인스턴스를 생성할 때 문서가 있는 디렉토리의 경로와 해당 문서를 식별할 수 있는 glob 패턴을 지정합니다.
 이 때 문서를 읽고 처리하기 위해서 UnstructuredLoader 가 내부적으로 사용됩니다. PDF, XML, HTML 등 미리 정의된 형식이 없는 텍스트 문서를 전처리하고 구조화된 형식으로 변환합니다. 이 로더를 사용하려면 unstructured 라이브러리가 설치되어 있어야 합니다.
